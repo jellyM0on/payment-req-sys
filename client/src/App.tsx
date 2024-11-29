d1f93e5420c332d9757a0e50a6a1148ff3aa68bc
jellyM0on <ylana.ong@gmail.com>
2024-11-29 21:35:18 +0800

    refactor(client): move browserRouter to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-
 import './App.css'
+import Login from './pages/login.tsx'
+import Signup from './pages/signup.tsx';
+import Dashboard from './pages/dashboard.tsx';
+import Home from './pages/home.tsx';
+import Request from './pages/request.tsx';
+
+import { Routes, Route, BrowserRouter } from "react-router";
+
 
 function App() {
 
   return (
-    <>
-     
-    </>
+     <BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/signup" element={<Signup/>}></Route>
+
+            <Route element={<Dashboard/>}>
+              <Route index element={<Home/>}></Route>
+              <Route path="/request" element={<Request/>}></Route>
+            </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './pages/login.tsx'
-import Signup from './pages/signup.tsx';
-import Dashboard from './pages/dashboard.tsx';
-import Home from './pages/home.tsx';
-import Request from './pages/request.tsx';
 
+import App from './App.tsx'
 
-import { Routes, Route, BrowserRouter } from "react-router";
 import "../node_modules/@freee_jp/vibes/vibes_2021.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/signup" element={<Signup/>}></Route>
-
-            <Route element={<Dashboard/>}>
-              <Route index element={<Home/>}></Route>
-              <Route path="/request" element={<Request/>}></Route>
-            </Route>
-            
-        </Routes>
-      </BrowserRouter>
+    <App/>
   </StrictMode>,
 )
